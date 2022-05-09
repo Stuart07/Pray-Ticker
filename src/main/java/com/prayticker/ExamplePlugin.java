03e255042edf450f9d8b260dacbfbd56b0e8f761
Stuart Mills <stuartmills1988@live.co.uk>
2022-05-09 21:58:27 +0100

    Test Commit

--- src/main/java/com/prayticker/ExamplePlugin.java
+++ src/main/java/com/prayticker/ExamplePlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.prayticker;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,7 +14,7 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Pray Ticker"
 )
 public class ExamplePlugin extends Plugin
 {
